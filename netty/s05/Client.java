083a3a3252a2d095af6228a429d4221c79b99e32
RMMerengue <1017703403@qq.com>
2021-12-07 15:45:47 +0800

    在Client中保存Client对象，窗口显示后调用connect连接，处理actionPerformed事件时调用send发送

---
+++ netty/s05/Client.java
@@ -0,0 +1,88 @@
+package com.msb.netty.s05;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+
+    private Channel channel = null;
+
+    public void connect() {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        Bootstrap b = new Bootstrap();
+
+        try {
+            ChannelFuture f = b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ClientChannelInitializer())
+                    .connect("localhost", 8888);
+
+            f.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if(!channelFuture.isSuccess()){
+                        System.out.println("not connected!");
+                    }else{
+                        System.out.println("connected!");
+                        channel = channelFuture.channel();
+                    }
+                }
+            });
+            f.sync();
+            System.out.println("...");
+
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public void send(String msg) {
+        ByteBuf buf = Unpooled.copiedBuffer(msg.getBytes());
+        channel.writeAndFlush(buf);
+    }
+
+    public static void main(String[] args) throws Exception{
+        Client c = new Client();
+        c.connect();
+    }
+}
+
+class ClientChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ch.pipeline().addLast(new ClientHandler());
+    }
+}
+
+class ClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ByteBuf buf = Unpooled.copiedBuffer("hello".getBytes());
+        ctx.writeAndFlush(buf);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try{
+            buf = (ByteBuf)msg;
+//            System.out.println(buf);
+//            System.out.println(buf.refCnt());
+        } finally {
+            if(buf != null) ReferenceCountUtil.release(buf);
+//            System.out.println(buf.refCnt());
+        }
+    }
+}
