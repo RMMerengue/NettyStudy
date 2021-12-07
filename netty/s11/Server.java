cae8dd09a8b59ee464d582b290df93659ed23f70
RMMerengue <1017703403@qq.com>
2021-12-07 20:16:04 +0800

    将ServerFrame设为单例

---
+++ netty/s11/Server.java
@@ -0,0 +1,86 @@
+package com.msb.netty.s11;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class Server {
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    public void serverStart() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+
+                            ChannelPipeline pl = ch.pipeline();
+                            pl.addLast(new ServerChildHandler());
+                        }
+                    })
+                    .bind(8888)
+                    .sync();
+            ServerFrame.INSTANCE.updateServerMsg("server started!");
+
+            f.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
+
+class ServerChildHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf) msg;
+
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(), bytes);
+            String s = new String(bytes);
+
+            if(s.equals("_bye_")) {
+                System.out.println("client closing");
+                Server.clients.remove(ctx.channel());
+                ctx.close();
+            }else{
+                Server.clients.writeAndFlush(msg);
+            }
+//            System.out.println(new String(bytes));
+//            System.out.println(buf);
+//            System.out.println(buf.refCnt());
+        } finally {
+//            if(buf != null) ReferenceCountUtil.release(buf);
+//            System.out.println(buf.refCnt());
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        Server.clients.remove(ctx.channel());
+        ctx.close();
+    }
+}
